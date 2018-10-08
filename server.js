432f28008321f1bf32a46e85d0263a7e4fad8edf
tmonty312 <tmonty312@gmail.com>
2018-10-08 14:20:45 -0600

    dirname updated

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build/index.html'));
+//   });
+
+app.use(express.static(__dirname))
 
 const port = 3112
 app.listen(port, ()=>console.log('I am listening on',port))
